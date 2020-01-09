290ee9698a854c26c189c52e80a047153e70aa81
Felipe <felipe.calderon@gigigo.com>
2020-01-09 17:15:12 +0100

    Try createServer

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const helmet = require("helmet");
 
 const express = require("express");
 const app = express();
-const http = require("http").Server(app);
+const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.use(bodyParser.urlencoded({ extended: false }));
